06889a0e2b90cffe7f59ab5a0cf4f4e4b17f233c
raulg-cpp <raulg.cpp@gmail.com>
2023-05-22 14:04:05 -0400

    better outline

--- src/App.js
+++ src/App.js
@@ -2,45 +2,57 @@ import './App.css';
 
 function App() {
   return (
-	<div className="App">
-    	{/* Inputs */}
-    	<div className="row my-1">
-    		<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    		<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    		<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-		</div>
-		
-		<div className="row">
-			{/* Left side */}
-			<div className="col">
-				{/* Numbers */}
-				<div className="row my-1">
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-				</div>
-				<div className="row my-1">
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-				</div>
-				<div className="row my-1">
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-				</div>
-				{/* Other */}
-				<div className="row my-1">
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-    				<button type="button" class="col mx-1 btn btn-primary">Primary</button>
-				</div>
+	<div className="App d-flex justify-content-center align-items-center">
+		<div className="container mainBox border border-4 border-secondary">
+			{/* Display */}
+			<div className="row mt-2 text-end">
+				<span className="text-warning fs-6">text</span>
+				<span className="text-light fs-5">text</span>
 			</div>
-			
-			{/* right side */}
-			<div className="col">
-				<button type="button" className="mx-1 btn btn-primary">Primary</button>
-				<button type="button" className="mx-1 btn btn-primary">Primary</button>
-				<button type="button" className="mx-1 btn btn-primary">Primary</button>
+		
+			{/* Buttons */}
+			<div className="row">
+				{/* left column */}
+				<div className="col">
+					{/* top operators */}
+					<div className="row mt-1">
+						<button type="button" class="mx-1 col btn btn-danger button">AC</button>
+						<button type="button" class="col-4 btn btn-info button">/</button>
+					</div>
+				
+					{/* Numbers */}
+					<div className="row mt-1">
+						<button type="button" class="mx-1 col btn btn-secondary button">7</button>
+						<button type="button" class="col btn btn-secondary button">8</button>
+						<button type="button" class="ms-1 col btn btn-secondary button">9</button>
+					</div>
+					<div className="row mt-1">
+						<button type="button" class="mx-1 col btn btn-secondary button">4</button>
+						<button type="button" class="col btn btn-secondary button">5</button>
+						<button type="button" class="ms-1 col btn btn-secondary button">6</button>
+					</div>
+					<div className="row mt-1">
+						<button type="button" class="mx-1 col btn btn-secondary button">3</button>
+						<button type="button" class="col btn btn-secondary button">2</button>
+						<button type="button" class="ms-1 col btn btn-secondary button">1</button>
+					</div>
+					
+					{/* Bottom operators */}
+					<div className="row my-1">
+						<button type="button" class="mx-1 col btn btn-secondary button">0</button>
+						<button type="button" class="col btn btn-secondary button">.</button>
+					</div>
+				</div>
+				
+				{/* right column */}
+				<div className="col-3 mx-1">
+		    		<div className="row">
+    					<button type="button" class="mt-1 btn btn-info button">x</button>
+    					<button type="button" class="my-1 btn btn-info button">-</button>
+    					<button type="button" class="mb-1 btn btn-info button">+</button>
+    					<button type="button" class="btn btn-info button">=</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
